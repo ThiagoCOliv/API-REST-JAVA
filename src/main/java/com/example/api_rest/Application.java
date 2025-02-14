4c54ca5d2f9c4ab12ea4c4507fbe57c3565179eb
Thiago Correa <thiagocdo445@gmail.com>
2025-02-14 13:58:55 -0300

    Finalização do projeto

--- src/main/java/com/example/api_rest/Application.java
+++ src/main/java/com/example/api_rest/Application.java
@@ -1,8 +1,11 @@
 package com.example.api_rest;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(servers = { @Server(url = "/", description = "URL padrão do servidor")})
 @SpringBootApplication
 public class Application 
 {
